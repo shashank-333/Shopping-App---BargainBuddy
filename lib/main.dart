0111f79d19de1d6270edb8344ed1c958f5030764
shashankbl <blshashank59@gmail.com>
2024-02-29 11:52:20 +0530

    Store the Secret key in saparate folder and  hide the keys file

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,10 @@
 import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_app_badger/flutter_app_badger.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
-import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:flutter_stripe/flutter_stripe.dart';
 import 'package:get/get.dart';
-import 'package:permission_handler/permission_handler.dart';
 import 'package:provider/provider.dart';
 import 'package:shoppingcarts/route.dart';
-import 'package:shoppingcarts/views/CartPage.dart';
-// import 'package:shopping/route.dart';
-// import 'package:responsive_login_ui/views/CartPage.dart';
-// import 'package:responsive_login_ui/views/signUp_view.dart';
 
 import 'IntialPage.dart';
 import 'Keys.dart';
